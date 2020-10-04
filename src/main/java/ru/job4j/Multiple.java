e571139caf6ae04846aac246c2fff1facca5d025
llota <llotua@mail.ru>
2020-10-05 00:38:32 +0300

    3.1. Загрузка файлов на github [#349251]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Multiple{
+    public static void main(String[] args) {
+        IntStream.range(2,10).forEach(i -> System.out.println(1 + " * " + i + " = " + 1*i));
+    }
+}
