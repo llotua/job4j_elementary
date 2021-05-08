02d70acc2e96aadd0730bc4daa1fda4428881bac
llota <llotua@mail.ru>
2021-05-08 15:18:02 +0300

    9. Площадь треугольника. [#349230]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -6,7 +6,7 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         idea += "But I am a newbie.";
-        int year = Year.now().getValue();
+        int year = 2021;
         idea += year;
         System.out.println(idea);
     }
