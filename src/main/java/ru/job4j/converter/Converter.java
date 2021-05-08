bf4e781975e06af492cffc9c9c60354bf8c5c601
llota <llotua@mail.ru>
2021-05-08 15:14:06 +0300

    9. Площадь треугольника. [#349230]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,21 +2,17 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static double rubleToEuro(int value) {
+    public static int rubleToEuro(int value) {
         return value / 70;
     }
 
-    public static double rubleToDollar(int value) {
+    public static int rubleToDollar(int value) {
         return value / 60;
     }
 
     public static void main(String[] args) {
-        System.out.println("140 rubles are " + String.format("%.2f", Converter.rubleToEuro(140)) + " euro.");
+        System.out.println("140 rubles are " + Converter.rubleToEuro(140) + " euro.");
 
-        System.out.println("180 rubles are " + String.format("%.2f", Converter.rubleToEuro(180)) + " euro.");
-
-        System.out.println("140 rubles are " + String.format("%.2f", Converter.rubleToDollar(140)) + " dollars.");
-
-        System.out.println("180 rubles are " + String.format("%.2f", Converter.rubleToDollar(180)) + " dollars.");
+        System.out.println("120 rubles are " + Converter.rubleToDollar(120) + " dollars.");
     }
 }
